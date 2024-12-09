83e316bd9a8afe40d991ffd6671828d8095a8cf3
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2024-12-09 15:16:32 +0400

    add next image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
-
+import profilePic from "./assets/men.jpg";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       Hello Next
+      <Image
+        src={profilePic}
+        alt="Picture of the author"
+        width={200}
+        draggable={false}
+        style={{
+          objectFit: "cover",
+          borderRadius: "50%",
+          minHeight: "200px",
+        }}
+        placeholder="blur"
+      />
     </div>
   );
 }
