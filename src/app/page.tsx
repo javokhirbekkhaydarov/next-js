7acf2693e25ccd0afd4bff8db2da4eb9a0bbb22a
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2024-12-15 11:02:27 +0400

    add info card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import profilePic from "./assets/men.jpg";
+import InfoCard from "@/app/components/InfoCard";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -16,6 +17,7 @@ export default function Home() {
         }}
         placeholder="blur"
       />
+        <InfoCard />
     </div>
   );
 }
