ebc846513ef649e3be40cb74ae5793541fc48cc4
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2024-12-15 12:45:15 +0400

    add video section

---
+++ src/app/video/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import VideoCall from "../components/video/VideoCall";
+
+const VideoPage = () => {
+    return (
+        <div>
+            <h1>Video Call Page</h1>
+            <VideoCall />
+        </div>
+    );
+};
+
+export default VideoPage;
